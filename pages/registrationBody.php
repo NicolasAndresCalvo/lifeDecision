c5db746013ac310f3fe67ccaeae9303eb276856c
nico calvo <nico.calvo2014@gmail.com>
2020-03-23 21:00:01 +0000

    Add title Registracion

--- pages/registrationBody.php
+++ pages/registrationBody.php
@@ -29,15 +29,48 @@
       <link href='https://fonts.googleapis.com/css?family=Open+Sans:600italic' rel='stylesheet' type='text/css'>    
       <script type="text/javascript" src="js/jquery.js"></script>
       <script type="text/javascript" src="js/Funcion.js"></script>
+      <style>
+        .btn .fas, .btn .fab, .btn .far {
+          margin-left: 3px;
+        }
+
+        h6 {
+          line-height: 1.7;
+        }
+
+        footer.page-footer {
+          margin-top: 0px;
+          padding-top: 0px;
+        }
+
+        body {
+          background-color: #eee;
+        }
+
+        .btn.peach-gradient {
+          color: #fff !important;
+        }
+
+        .map-container-section {
+          overflow:hidden;
+          /* padding-bottom:56.25%; */
+          position:relative;
+          height:0;
+        }
+        .map-container-section iframe {
+          left:0;
+          top:0;
+          height:100%;
+          width:100%;
+          position:absolute;
+        }
+      </style>
   </head>
   <body id="colorIniciar">
     <!-- Habia 5 br -->
-    <br><br><br>
-
-
-
-
-
+    <br><br><br><br><br>
+    <!--Main Layout-->
+    <main>
       <div class="container-fluid mb-5">
         <!--Grid row-->
         <div class="row" style="margin-top: -100px;">
@@ -45,6 +78,10 @@
           <div class="col-md-12">
             <div class="card pb-5">
               <div class="card-body">
+                <h2 class="font-weight-bold my-4 text-center font-weight-bold">
+                  <strong>REGISTRACION</strong>
+                </h2>
+                <hr>
                 <!--Start Form-->
                 <div class="container">
                   <div class="row">
@@ -59,6 +96,10 @@
                     <div class="col-md-12">
                       <form id="Registracion" class="" action="ConsultaRegistracion.php" method="post">
                         <div class="row">
+                          <!--Section description-->
+                          <p class="text-center grey-text mb-5 mx-auto w-responsive">Lorem ipsum dolor sit amet, consectetur
+                          adipisicing elit. Fugit, error amet numquam iure provident voluptate
+                          esse quasi, veritatis totam voluptas nostrum quisquam eum porro a pariatur accusamus veniam.</p>
                           <!--Grid column-->
                           <div class="col-md-6">
                             <div class="md-form">
@@ -126,10 +167,8 @@
         <!--Grid row-->
         </div>
       </div>
-
-
-
-
+    </main>
+    <!--Main Layout-->
     <!--  SCRIPTS  -->
     <!-- JQuery -->
     <script type="text/javascript" src="../js/jquery.min.js"></script>
